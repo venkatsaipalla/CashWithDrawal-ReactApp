d33f679f992b7b73df6eccd13503a83941706890
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2022-11-26 00:26:58 +0530

    Add files via upload

---
+++ src/components/CashWithdrawal/index.js
@@ -0,0 +1,59 @@
+import {Component} from 'react'
+
+import DenominationItem from '../DenominationItem'
+
+import './index.css'
+
+class CashWithdrawal extends Component {
+  state = {
+    balance: 2000,
+  }
+
+  updateBalance = value => {
+    this.setState(prevState => ({balance: prevState.balance - value}))
+  }
+
+  render() {
+    const {denominationsList} = this.props
+    const {balance} = this.state
+    const name = 'Palla VenkataSai'
+
+    return (
+      <div className="app-container">
+        <div className="cash-withdrawal-container">
+          <div className="user-details-container">
+            <div>
+              <img
+                className="initial-container"
+                src="https://res.cloudinary.com/dxejhgtqt/image/upload/r_8/v1669401564/samples/CompressJPEG.online_500x500-image_nzu9za.jpg"
+                alt="venkat sai"
+              />
+            </div>
+            <p className="name">{name}</p>
+          </div>
+          <div className="balance-container">
+            <p className="your-balance">Your Balance</p>
+            <p className="balance">
+              {balance}
+              <br />
+              <span className="currency">In Rupees</span>
+            </p>
+          </div>
+          <p className="withdraw">Withdraw</p>
+          <p className="choose-sum">CHOOSE SUM (IN RUPEES)</p>
+          <ul className="denominations-list">
+            {denominationsList.map(eachDenomination => (
+              <DenominationItem
+                key={eachDenomination.id}
+                denominationDetails={eachDenomination}
+                updateBalance={this.updateBalance}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CashWithdrawal
